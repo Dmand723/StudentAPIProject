b321ad748ba975cf3142552cfbb7a494e6956679
Dmand723 <dawson.simmons723@gmail.com>
2024-12-16 10:33:08 -0700

    Finished Part 3

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const routes = require("express").Router();
 routes.get("/", myController.awsomeFunction);
 routes.get("/ttech", myController.ttechFunction);
 routes.get("/whatsThis", myController.dawsonFunction);
-routes.get("/students", myController.getAllStudents);
+routes.get("/studentsTest", myController.getAllStudents);
+
+//student routes
+routes.use("/students", require("./students"));
 module.exports = routes;
